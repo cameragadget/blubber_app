bc3ccc3864e93ea4df25f68a4007a42f497bcbec
jonah sobol <jonah.sobol@gmail.com>
2016-03-04 11:29:59 -0800

    show users

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,17 @@
+var User = require('../models/User')
+
 function index(req, res, next) {
-  res.send('respond with a resource');
+  User.find({}, function(err, users){
+    res.send(users);
+  });
 };
 
 function show(req, res, next) {
-  res.send('show a user!');
+  var id = req.params.id;
+
+  User.findById(id, function(err, user){
+    res.send(user);
+  });
 };
 
 
